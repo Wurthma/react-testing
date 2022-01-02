6e3f3076d61f33cb7c5f69cef557808dbcffabc1
George Henrique Wurthmann <gemakai@gmail.com>
2022-01-02 14:30:29 -0300

    Exercise: disable or enable button when mark checkbox

--- 2-color-button/src/App.test.tsx
+++ 2-color-button/src/App.test.tsx
@@ -6,7 +6,7 @@ test('button has corret initial color', () => {
   render(<App />);
   const colorButton = screen.getByRole('button', { name: 'Change to blue' });
 
-  expect(colorButton).toHaveStyle({ backgroundColor: 'red' })
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
 });
 
 test('button turn blue when clicked', () => {
@@ -17,4 +17,31 @@ test('button turn blue when clicked', () => {
   expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
   
   expect(colorButton.textContent).toBe('Change to red');
+});
+
+test('initial conditions', () => {
+  render(<App />);
+
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+
+  expect(colorButton).toBeEnabled();
+
+  const checkbox = screen.getByRole('checkbox');
+
+  expect(checkbox).not.toBeChecked();
+});
+
+test('Checkbox disables on first click and enables on second click', () => {
+  render(<App />);
+
+  const checkbox = screen.getByRole('checkbox');
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+
+  fireEvent.click(checkbox);
+  expect(checkbox).toBeChecked();
+  expect(colorButton).toBeDisabled();
+
+  fireEvent.click(checkbox);
+  expect(checkbox).not.toBeChecked();
+  expect(colorButton).toBeEnabled();
 });
--- 2-color-button/src/App.tsx
+++ 2-color-button/src/App.tsx
@@ -5,14 +5,25 @@ import './App.css';
 function App() {
   const [buttonColor, setButtonColor] = useState('red');
   const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
+
+  const [checkboxState, setCheckboxState] = useState(false);
+
   return (
     <div>
-      <button 
+      <button
         style={{ backgroundColor: buttonColor }}
         onClick={() => setButtonColor(newButtonColor)}
+        disabled={checkboxState}
       >
         Change to {newButtonColor}
-        </button>
+      </button>
+      <input
+        type="checkbox"
+        id="enable-button-checkbox"
+        defaultChecked={checkboxState}
+        aria-checked={checkboxState}
+        onClick={() => setCheckboxState(!checkboxState)}
+      />
     </div>
   );
 }
