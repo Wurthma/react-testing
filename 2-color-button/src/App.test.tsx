19045d914c66f88024510c5290d139990d880b56
George Henrique Wurthmann <gemakai@gmail.com>
2022-01-02 09:59:53 -0300

    Button color changes and tests with TDD

--- 2-color-button/src/App.test.tsx
+++ 2-color-button/src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('button has corret initial color', () => {
@@ -10,5 +10,11 @@ test('button has corret initial color', () => {
 });
 
 test('button turn blue when clicked', () => {
+  render(<App />);
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  fireEvent.click(colorButton);
+
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
   
+  expect(colorButton.textContent).toBe('Change to red');
 });
